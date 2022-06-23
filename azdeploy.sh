2fc5b9b32547e50b2c5bb4bc8c8ed082416a58ce
ozunuane <ozunuane@gmail.com>
2022-06-23 16:59:53 +0100

    version2

--- azdeploy.sh
+++ azdeploy.sh
@@ -9,7 +9,7 @@ echo '##############################################################'
 az deployment group create \
 --resource-group  NT-Prod-Platform-Partner-RG \
 --template-file documentsvc.bicep \
---parameters service='prodplat80-Test'  \
+--parameters service='prodplat81-Test'  \
   --confirm-with-what-if \
 
 echo '#################################################x#############'
